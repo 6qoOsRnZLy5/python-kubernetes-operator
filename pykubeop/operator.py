04161324b67620ee1d2ec4bd611419c85a725504
6qoOsRnZLy5 <49039264+6qoOsRnZLy5@users.noreply.github.com>
2019-03-28 21:03:45 +0100

    test

--- pykubeop/operator.py
+++ pykubeop/operator.py
@@ -11,29 +11,19 @@ class KubernetesOperator(object):
         parser = argparse.ArgumentParser(crd.KIND)
         parser.add_argument(
             "--scope",
-            default="cluster",
+            default=os.environ["cfg_scope"],
             choices=["cluster", "namespaced"],
             help="Cluster or Namespace Scoped?"
         )
         parser.add_argument(
             "--namespace",
-            default=None,
+            default=os.environ["cfg_namespace"],
             help="Which namespace to monitor for {} objects".format(crd.KIND)
-        )
-        parser.add_argument(
-            "--create-crd",
-            action="store_true",
-            help="Create the CustomResourceDefinition on startup if missing"
-        )
-        parser.add_argument(
-            "--external",
-            action="store_true",
-            help="Run outside of the Kubernetes Cluster"
         )
         parser.add_argument(
             "-L",
             "--log-level",
-            default="INFO",
+            default=os.environ["cfg_loglevel"],
             choices=["DEBUG", "INFO", "WARNING", "ERROR"]
         )
         self.add_arguments(parser)
@@ -67,23 +57,6 @@ class KubernetesOperator(object):
 
     def run(self):
         self.logger.info("Starting Kubernetes Operator")
-        if self.args.create_crd:
-            self.logger.info(
-                "Checking for CustomResourceDefintion for %s", self.crd.KIND
-            )
-            if not self.crd.get_custom_resource_definition(self.api_client):
-                self.logger.info(
-                    "Creating CustomResourceDefintion: %s", self.crd.API_GROUP
-                )
-                self.crd.create_custom_resource_definition(self.api_client)
-                self.logger.info(
-                    "CustomResourceDefinition %s created", self.crd.API_GROUP
-                )
-            else:
-                self.logger.info(
-                    "CustomResourceDefinition %s already exists", self.crd.API_GROUP
-                )
-
         self.logger.info("Watching Kubernetes API for new %s Resources", self.crd.KIND)
         running = True
         while running:
